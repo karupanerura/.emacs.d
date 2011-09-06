abb7a2864b1915309144e65d9d321c9056d0835c
Kenta Sato <k-sato@mfac.jp>
2011-09-06 17:05:11 +0900

    initalize commit

---
+++ init.el
@@ -0,0 +1,151 @@
+;;; Localeに合わせた環境の設定
+(set-locale-environment nil)
+;; UTF-8 and Japanese Setting
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(setq file-name-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8-unix)
+(setq default-input-method 'japanese-anthy)
+
+;;; emacs lisp を読み込む
+(setq load-path (cons "~/.emacs.d/elisp" load-path))
+(setq load-path (cons "~/.emacs.d/elisp/apel" load-path))
+
+;;; タブ幅の設定
+(setq-default tab-width 4 indent-tabs-mode nil)
+(setq-default perl-basic-offset 4)
+(setq-default html-basic-offset 2)
+
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\|t\\|cgi\\|psgi\\)\\'" . perl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . perl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . perl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . perl-mode))
+
+;;; 対応する括弧を光らせる。
+(show-paren-mode 1)
+;;; ウィンドウ内に収まらないときだけ括弧内も光らせる。
+(setq show-paren-style 'mixed)
+;;; 行末の空白を表示
+(setq-default show-trailing-whitespace t)
+;;; 現在行を目立たせる
+(global-hl-line-mode)
+;;; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+;;; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+;;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+;;; バックアップファイルを作らない
+(setq backup-inhibited t)
+;;; gzファイルも編集できるようにする
+(auto-compression-mode t)
+
+;;; ファイル名が重複していたらディレクトリ名を追加する。
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+;;; 自動セーブするようにする
+(require 'auto-save-buffers)
+(run-with-idle-timer 0.5 t 'auto-save-buffers)
+
+;;; diredを便利にする
+(require 'dired-x)
+
+;;; diredから"r"でファイル名をインライン編集する
+(require 'wdired)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+;;; diredで"V"を入力すると
+;;; そのディレクトリで使っているバージョン管理システム用のモードを起動します。
+(defun dired-vc-status (&rest args)
+  (interactive)
+  (cond ((file-exists-p (concat (dired-current-directory) ".svn"))
+         (svn-status (dired-current-directory)))
+        ((file-exists-p (concat (dired-current-directory) ".git"))
+         (magit-status (dired-current-directory)))
+        (t
+         (message "version controlled?"))))
+
+(define-key dired-mode-map "V" 'dired-vc-status)
+
+;;; 再帰的にgrep
+(require 'grep)
+(setq grep-command-before-query "grep -nH -r -e ")
+(defun grep-default-command ()
+  (if current-prefix-arg
+      (let ((grep-command-before-target
+             (concat grep-command-before-query
+                     (shell-quote-argument (grep-tag-default)))))
+        (cons (if buffer-file-name
+                  (concat grep-command-before-target
+                          " *."
+                          (file-name-extension buffer-file-name))
+                (concat grep-command-before-target " ."))
+              (+ (length grep-command-before-target) 1)))
+    (car grep-command)))
+(setq grep-command (cons (concat grep-command-before-query " .")
+                         (+ (length grep-command-before-query) 1)))
+
+; perl tidy
+; sudo aptitude install perltidy
+(defun perltidy-region ()
+  "Run perltidy on the current region."
+  (interactive)
+  (save-excursion
+    (shell-command-on-region (point) (mark) "perltidy -q" nil t)))
+(defun perltidy-defun ()
+  "Run perltidy on the current defun."
+  (interactive)
+  (save-excursion (mark-defun)
+                  (perltidy-region)))
+(global-set-key "\C-ct" 'perltidy-region)
+(global-set-key "\C-c\C-t" 'perltidy-defun)
+
+;; flymake for perl
+(defvar flymake-perl-err-line-patterns '(("\\(.*\\) at \\([^ \n]+\\) line \\([0-9]+\\)[,.\n]" 2 3 nil 1)))
+(defconst flymake-allowed-perl-file-name-masks '(("\\.pl$" flymake-perl-init)
+                                               ("\\.pm$" flymake-perl-init)
+                                               ("\\.t$" flymake-perl-init)
+                                               ))
+
+(defun flymake-perl-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "perl" (list "-wc" local-file))))
+
+(defun flymake-perl-load ()
+  (interactive)
+  (set-perl5lib)
+  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check)
+  (setq flymake-allowed-file-name-masks (append flymake-allowed-file-name-masks flymake-allowed-perl-file-name-masks))
+  (setq flymake-err-line-patterns flymake-perl-err-line-patterns)
+  (flymake-mode t))
+
+(add-hook 'perl-mode-hook '(lambda () (flymake-perl-load)))
+
+(defun next-flymake-error ()
+  (interactive)
+  (flymake-goto-next-error)
+  (let ((err (get-char-property (point) 'help-echo)))
+    (when err
+      (message err))))
+(global-set-key "\C-ce" 'next-flymake-error)
+
+(autoload 'php-mode "php-mode")
+(setq auto-mode-alist
+      (cons '("\\.php\\'" . php-mode) auto-mode-alist))
+(setq php-mode-force-pear t)
+(add-hook 'php-mode-user-hook
+  '(lambda ()
+     (setq php-manual-path "/usr/local/share/php/doc/html")
+     (setq php-manual-url "http://www.phppro.jp/phpmanual/")))
