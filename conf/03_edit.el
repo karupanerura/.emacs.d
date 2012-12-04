7480724b9bbcb40a5b0d4ba0b00523be5e7aa07d
karupanerura <karupa@cpan.org>
2012-12-04 12:19:08 +0900

    こうしないと日本語の扱いがおかしいっぽい

--- conf/03_edit.el
+++ conf/03_edit.el
@@ -7,16 +7,17 @@
  '(find-file-visit-truename t)              ; visit truename
  '(read-file-name-completion-ignore-case t) ; for case insensitive find-file
  '(dired-use-ls-dired nil)                  ; dired ls
- '(file-name-coding-system  'utf-8))        ; file-name-coding-system
+ '(file-name-coding-system  'utf-8)         ; file-name-coding-system
+ '(default-input-method 'japanese-anthy))   ; default-input-method
 
 ;; UTF-8 and Japanese Setting
-(prefer-coding-system          'utf-8)
 (set-locale-environment        nil)
 (set-language-environment      "Japanese")
 (set-terminal-coding-system    'utf-8)
 (set-keyboard-coding-system    'utf-8)
 (set-buffer-file-coding-system 'utf-8-unix)
 (set-default-coding-systems    'utf-8)
+(prefer-coding-system          'utf-8)
 (set-clipboard-coding-system   'utf-8)
 
 ;; find-file-at-point
