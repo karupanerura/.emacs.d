8d4f4e034a27e370a901287b8dcd97ef05c8863d
karupanerura <karupa@cpan.org>
2013-03-11 01:03:31 +0900

    yagist.el is depend on json.el 1.2

---
+++ vendor/elpa/json-1.2/json-autoloads.el
@@ -0,0 +1,19 @@
+;;; json-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/vendor/elpa/json-1.2/json-pkg.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/json-1.2/json.el") (20796
+;;;;;;  44687 38368))
+
+;;;***
+
+(provide 'json-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; json-autoloads.el ends here
