63f98653f72557d0c62da9e2812d7f850cebf87f
karupanerura <karupa@cpan.org>
2013-07-10 18:59:52 +0900

    updated modules.

---
+++ vendor/elpa/auto-complete-20130503.2013/auto-complete-autoloads.el
@@ -0,0 +1,31 @@
+;;; auto-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (auto-complete) "auto-complete" "../../../../../.emacs.d/vendor/elpa/auto-complete-20130503.2013/auto-complete.el"
+;;;;;;  "901a3d1106ca99b08654979a9bc8ebd6")
+;;; Generated autoloads from ../../../../../.emacs.d/vendor/elpa/auto-complete-20130503.2013/auto-complete.el
+
+(autoload 'auto-complete "auto-complete" "\
+Start auto-completion at current point.
+
+\(fn &optional SOURCES)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/vendor/elpa/auto-complete-20130503.2013/auto-complete-config.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/auto-complete-20130503.2013/auto-complete-pkg.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/auto-complete-20130503.2013/auto-complete.el")
+;;;;;;  (20957 12068 194580 639000))
+
+;;;***
+
+(provide 'auto-complete-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-complete-autoloads.el ends here
