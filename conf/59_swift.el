0c59188becf09aaca536184f7993834c0729dbf1
karupanerura <karupa@cpan.org>
2015-05-13 15:50:44 +0900

    swift-flycheck only swift-mode.

--- conf/59_swift.el
+++ conf/59_swift.el
@@ -1,3 +1,7 @@
 ;; swift
 (add-to-list 'auto-mode-alist '("\\.swift$" . swift-mode))
-(add-to-list 'flycheck-checkers 'swift)
+(autoload 'flycheck-select-checker "flycheck" nil t)
+(defun my-swift-mode-hook ()
+  "Hook function for 'swift-mode'."
+  (add-to-list 'flycheck-checkers 'swift))
+(add-hook 'swift-mode-hook 'my-swift-mode-hook)
