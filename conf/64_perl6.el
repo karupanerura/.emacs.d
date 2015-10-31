85774d6f9d86d933dce8c2b9643dd89f6922fefb
karupanerura <karupa@cpan.org>
2015-10-31 22:11:09 +0900

    added perl6-mode

---
+++ conf/64_perl6.el
@@ -0,0 +1,3 @@
+;;; Perl6
+(add-to-list 'auto-mode-alist '("\\.pm6$" . perl6-mode))
+(add-to-list 'auto-mode-alist '("\\.pl6$" . perl6-mode))
---
+++ vendor/elpa/perl6-mode-20151014.1518/perl6-mode-autoloads.el
@@ -0,0 +1,57 @@
+;;; perl6-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "perl6-detect" "../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-detect.el"
+;;;;;;  "cb162092c8ec4091e4f3ceaf5ff00d07")
+;;; Generated autoloads from ../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-detect.el
+
+(add-to-list 'interpreter-mode-alist '("perl6" . perl6-mode))
+
+(add-to-list 'auto-mode-alist '("\\.p[lm]?6\\'" . perl6-mode))
+
+(defconst perl6-magic-pattern (rx line-start (0+ space) (or (and "use" (0+ space) "v6") (and (opt (and (or "my" "our") (1+ space))) (or "module" "class" "role" "grammar" "enum" "slang" "subset")))))
+
+(autoload 'perl6-magic-matcher "perl6-detect" "\
+Check if the current buffer is a Perl 6 file.
+
+Only looks at a buffer if it has a file extension of .t, .pl, or .pm.
+
+Scans the buffer (to a maximum of 4096 chars) for the first non-comment,
+non-whitespace line.  Returns t if that line looks like Perl 6 code,
+nil otherwise.
+
+\(fn)" nil nil)
+
+(add-to-list 'magic-mode-alist '(perl6-magic-matcher . perl6-mode))
+
+;;;***
+
+;;;### (autoloads nil "perl6-mode" "../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-mode.el"
+;;;;;;  "f8036f369993a6cf77eac01554763916")
+;;; Generated autoloads from ../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-mode.el
+
+(autoload 'perl6-mode "perl6-mode" "\
+Major mode for editing Perl 6 code.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-detect.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-font-lock.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-indent.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-mode-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-mode-pkg.el"
+;;;;;;  "../../../../../.emacs.d/vendor/elpa/perl6-mode-20151014.1518/perl6-mode.el")
+;;;;;;  (22068 48067 344359 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; perl6-mode-autoloads.el ends here
